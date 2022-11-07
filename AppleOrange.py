732cf6791c5c5421a66b1af8e7420c94df1c1d06
Sk khalikul <108520856+Khalikul@users.noreply.github.com>
2022-11-07 12:16:38 +0530

    Update AppleOrange.py

--- AppleOrange.py
+++ AppleOrange.py
@@ -1,1 +1,12 @@
-
+t = 0
+t1 = 0
+for i in range(len(apples)):
+    temp = a + apples[i]
+    if (temp in range(s, t + 1)):
+        t += 1
+for i in range(len(oranges)):
+    temp = b + oranges[i]
+    if (temp in range(s, t + 1)):
+        t1 += 1
+print(t)
+print(t1)
